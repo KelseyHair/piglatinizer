bbb544ccb1c46686ad9d95f2b56c216936168a07
KelseyHair <Sheliashate@gmail.com>
2019-10-30 22:25:48 +0000

    worked towards display

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+console.log("hi")
+
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+$("#button").click(function(){
+    let message =
+    $(".input").val();
+    console.log(message)
+
+    var consonant = "ay";
+    var vowel = "yay";
+
+    if{
 
 
-  
-  
-  
-  
+    }
+
+
+
+
+
+
+
+
+
+
+
+});
 });
